187156d95324f514d332e95a5a85897fa4a3e66f
Echo <89703312@qq.com>
2019-04-17 11:00:08 +0800

    字典——OrderedDict
    可以用OrderedDict实现一个FIFO定容的字典

--- Dict.py
+++ Dict.py
@@ -1,6 +1,6 @@
-from collections import OrderedDict, Counter, defaultdict
+from collections import OrderedDict, _Link, _proxy
 
-# OrderedDict
+# 1. OrderedDict
 # 使用dict时，键值对是无序的，在对dict遍历时，无法确定key的顺序
 # 若需要保持key的顺序，可以使用OrderedDict
 # OrderedDict本质上是一个环形的双端链表，头和尾用一个 Sentinel 来表示
@@ -14,3 +14,22 @@ print(d)
 
 od = OrderedDict(zip(keys, values))
 print(od)
+
+# 可以使用OrderedDict来实现一个定容的FIFO的dict
+class LastUpdatedOrderedDict(OrderedDict):
+
+    def __init__(self, capacity):
+        super(LastUpdatedOrderedDict, self).__init__()
+        self._capacity = capacity
+
+    def __setitem__(self, key, value, dict_setitem=dict.__setitem__, proxy=_proxy, Link=_Link):
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove:', last)
+        if containsKey:
+            del self[key]
+            print('set:', (key, value))
+        else:
+            print('add:', (key, value))
+        OrderedDict.__setitem__(self, key, value)
