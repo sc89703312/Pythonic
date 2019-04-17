ebc8188c81dc36ceb77f7e819cd90598a9f33227
Echo <89703312@qq.com>
2019-04-17 20:40:25 +0800

    tutorial itertools
    组合 排列 笛卡尔积 生成器等

---
+++ built-in lib/tutorial_itertool.py
@@ -0,0 +1,94 @@
+from itertools import accumulate, count
+from operator import mul
+
+# ===========================================================================================
+# 1. accumulate(iterable[, func])
+# func接受二元参数
+print(list(accumulate([1,2,3,4,5])))
+print(list(accumulate([1,2,3,3,5], func=mul)))
+print(list(accumulate([2,1,3,2,5], func=min)))
+print(list(accumulate([2,1,3,2,5], func=max)))
+
+# ===========================================================================================
+# 2. 组合
+def combinations(iterable, r):
+    # 不放回的拿
+    # 保证indices是一个单调递增的位置序列
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+
+        yield tuple(pool[i] for i in indices)
+
+def combinations_with_replacement(iterable, r):
+    # 有放回的拿
+    # 保证indices是一个单调不减的位置序列
+    # combinations_with_replacement('ABC', 2) --> AA AB AC BB BC CC
+    pool = tuple(iterable)
+    n = len(pool)
+    if not n and r:
+        return
+    indices = [0] * r
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != n - 1:
+                break
+        else:
+            return
+        indices[i:] = [indices[i] + 1] * (r - i)
+        yield tuple(pool[i] for i in indices)
+
+
+# ===========================================================================================
+# 3. 排列
+def permutations(iterable, r=None):
+    # permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
+    # permutations(range(3)) --> 012 021 102 120 201 210
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = list(range(n))
+    cycles = list(range(n, n-r, -1))
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
+
+# ===========================================================================================
+# 4. 笛卡尔积
+def product(*args, repeat=1):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = [tuple(pool) for pool in args] * repeat
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
