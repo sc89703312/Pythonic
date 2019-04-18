be99ed2f75b1cdba53473592c60e5732a3ebe592
Echo <89703312@qq.com>
2019-04-18 18:53:22 +0800

    functools
    LRU_CACHE
    @wrap()

---
+++ built-in lib/tutorial_functools.py
@@ -0,0 +1,29 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+test = [fib(i) for i in range(10)]
+print(test)
+print(fib.cache_info())
+
+
+from functools import wraps
+def my_decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        print('Calling decorated function')
+        return f(*args, **kwds)
+    return wrapper
+
+@my_decorator
+def example():
+    """Docstring"""
+    print('Called example function')
+
+example()
+print(example.__name__)
+print(example.__doc__)
